80d7a4e82e80f9d838654b6edcf86b9df6a8ce6e
Ando_lalanne <lalanne.andoni1@gmail.com>
2023-04-27 10:27:52 +0200

    J'ai lié mon fichier javascript à l'HTML et j'ai un peu avancé sur le js

--- test.js
+++ test.js
@@ -1,9 +1,10 @@
 // Etape 1 - Sélectionner nos éléments
-//let input = document.querySelector('input');
-//let bouton = document.querySelector('button');
+let input = document.querySelector('#prix');
+let formulaire = document.querySelector('#formulaire');
+let error =  document.querySelector('#small');
 
 // Etape 2 - Cacher l'erreur
-
+error.style.display = "none";
 // Etape 3 - Générer un nombre aléatoire
 
 // Etape 4 - Vérifier que l'utilisateur donne bien un nombre
