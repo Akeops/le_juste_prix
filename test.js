ff0c321745588c35696fa14dac23d465a2dfe2a4
Ando_lalanne <lalanne.andoni1@gmail.com>
2023-04-28 11:38:02 +0200

    j'ai mis du dynamique sur la page via le js, je viens de mettre un timer sur le style qui apparait sur le input quand on clique sur le bouton 'deviner'

--- test.js
+++ test.js
@@ -5,9 +5,42 @@ let error =  document.querySelector('#small');
 
 // Etape 2 - Cacher l'erreur
 error.style.display = "none";
-// Etape 3 - Générer un nombre aléatoire
+
+
+// Etape 3 - Générer un nombre aléatoire entre 0 et 1000
+let nombreAleatoire = Math.floor(Math.random() * 1001);
+
 
 // Etape 4 - Vérifier que l'utilisateur donne bien un nombre
+input.addEventListener('keyup', () => {
+    if(isNaN(input.value)){
+        error.style.display = "inline";
+        error.style.color = "#E74646";
+    }
+    else {
+        error.style.display = "none";
+    }
+});
+
+formulaire.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    if(!isNaN(input.value) && input.value == ""){
+        input.style.borderColor = "#E74646"
+    }
+    else {
+        input.style.borderColor = "green";
+    }
+});
+
+formulaire.addEventListener('click', function() {
+
+    setTimeout(function() {
+        input.style.borderColor = "";
+      }, 200); // La couleur de fond disparaîtra après 100 millisecondes
+})
+
+
 
 // Etape 5 - Agir à l'envoi du formulaire
 
